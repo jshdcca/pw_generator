b695ef7160634d03c7569f46dc99694e6ef63ac2
Josh Castillo <jshdcca@gmail.com>
2020-04-15 17:07:42 +0800

    mv main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from __future__ import print_function
 from flask import Flask
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World'
+def pgen():
+	with open('words_alpha.txt') as f:
+		word = f.readlines()
+		passwordlist = []
+		for _ in range(4): passwordlist.append(random.choice(word))
+		pw = []
+		for p in passwordlist: pw.append(p.strip())
+		return (" ".join(pw))
+	
 
 
